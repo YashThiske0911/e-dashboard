e78f912785f2fcf6cdcd5feae3a3240141b88fa3
YashThiske0911 <thiskeyash@gmail.com>
2023-12-13 14:54:56 +0530

    Routing in Raectjs (Install Router/make header file/make links/make routes)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from './Nav'
+import { BrowserRouter, Routes ,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>e-dashboard</h1>
+      <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<h1>Product listing component</h1>} />
+        <Route path="/add" element={<h1>Add product component</h1>} />
+        <Route path="/update" element={<h1>Update product component</h1>} />
+        <Route path="/logout" element={<h1>Logout product component</h1>} />
+        <Route path="/profile" element={<h1>Product product component</h1>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
