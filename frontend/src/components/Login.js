d405b35e00f57265b34ebe6b5ba74c0002d1e5d8
YashThiske0911 <thiskeyash@gmail.com>
2023-12-14 14:45:58 +0530

    login API integration

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {  useNavigate } from "react-router-dom";
 
 const Login=()=>{
     const [email,setEmail]= React.useState('');
     const [password,setPassword]= React.useState('');
-    const handelLogin=()=>{
+    const navigate= useNavigate();
+    useEffect(()=>{
+        const auth=localStorage.getItem('user');
+        if(auth){
+            navigate("/")
+        }
+    })
+    const handelLogin=async()=>{
         console.warn(email,password);
+        let result= await fetch('http://localhost:5000/login',{
+            method:'post',
+            body:JSON.stringify({email,password}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        result = await result.json();
+        console.warn(result);
+        if(result.name){
+            localStorage.setItem("user",JSON.stringify(result));
+            navigate("/")
+
+        }else{
+            alert("please enter correct details")
+        }
     }
     return(
         <div className="login">
+            <h1>Login</h1>
             <input className="inputBox" type="text" placeholder="Enter Email" 
             onChange={(e)=>setEmail(e.target.value)} value={email} />
             <input className="inputBox" type="password" placeholder="Enter Password" 
