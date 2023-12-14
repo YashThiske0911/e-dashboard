d66648e47fd67668ed654add773736d70974c025
YashThiske0911 <thiskeyash@gmail.com>
2023-12-14 12:11:03 +0530

    Login API

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const cors=require('cors');
-require('./db/config');
-const User= require("./db/User");
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/register",async(req,resp)=>{
-    let user=new User(req.body);
-    let result = await user.save();
-    resp.send(req.body)
-})
+app.post("/register", async (req, resp) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  resp.send(req.body);
+});
 
-app.listen(5000)
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User Found" });
+    }
+  } else {
+    resp.send({ result: "No User Found" });
+  }
+});
+
+app.listen(5000);
